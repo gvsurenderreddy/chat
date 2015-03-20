137de0ae31784bc8d8cc2150b40e87ff59b023a2
kwentinn <kwentinn@gmail.com>
2015-03-20 17:06:33 +0100

    Dernieres modifs
    - AuthManager : gestion des erreurs
    - Attention à la sauvegarde du profil utilisateur (fichiers
    user-profile.js, et lib/app-routes.js)...

--- js/user-profile.js
+++ js/user-profile.js
@@ -1,78 +1,45 @@
-var imageData = null;
+var data = userProfile || {}; // équivalent de userProfile ?? new Object(); en c#.
+
+//console.dir(data);
 
 var displayAlert = function (css, title, message) {
-
 	$('#alert-area').html('<div class="alert alert-' + css + ' alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>' + title + '</strong>&nbsp;' + message + '</div>');
 }
 
-// $('#btn-save-profile').click(function (e) {
+//$('#btn-save-profile').click(function (e) {
+$('form').submit(function (e) {
+	e.preventDefault();
+	
+	/** Enregistrement de l'image du profil utilisateur. */
+	var formData = new FormData();
+	try{
+		formData.append('file', $('input:file').files[0]);
+	}catch(ex){
+		console.dir(ex);
+	}
+	
+	$.ajax({
+		url : '/userProfile',
+		dataType : 'json',
+		data : formData,
+		processData : false,
+		contentType : false,
+		type : 'POST',
+		mimeType: 'multipart/form-data',
+		success : function (data) {
+			if (data.error != null) {
+				displayAlert('danger', 'Erreur !', data.error);
+			} else {
+				displayAlert('success', 'OK !', data.message);
+			}
+		},
+		error : function (jqXHR, textStatus, errorThrown) {
+			displayAlert('danger', 'Erreur!', textStatus);
+		}
+	});
 
-// var firstname = $('#firstname').val();
-// var lastname = $('#lastname').val();
-// var dateOfBirth = $('#date-of-birth').datepicker('getDate');
-// var country = $('#country').val();
-// var email = $('#email').val();
-
-// /** Enregistrement de l'image du profil utilisateur. */
-// if (imageData != null) {
-// $.ajax({
-// url : '/user-image',
-// dataType : 'json',
-// //contentType : 'application/octet-stream',
-// //processData : false,
-// data : imageData,
-// type : 'POST',
-// success : function (data) {
-// if (data.error != null) {
-// displayAlert('danger', 'Erreur !', data.error);
-// } else {
-// displayAlert('success', 'OK !', data.message);
-// }
-// },
-// error : function (jqXHR, textStatus, errorThrown) {
-// displayAlert('danger', 'Erreur!', textStatus);
-// }
-// });
-// }
-
-// /** Enregistrement des autres infos du profil utilisateur. */
-// // if (!(firstname == '' && lastname == '' && dateOfBirth == '' && country == '' && email == '')) {
-// // var formData = {
-// // 'firstname' : firstname,
-// // 'lastname' : lastname,
-// // 'date-of-birth' : dateOfBirth,
-// // 'country' : country,
-// // 'email' : email
-// // //'avatar-image' : imageData
-// // }
-
-// // $.ajax({
-// // url : '/userProfile',
-// // dataType : 'json',
-// // data : formData,
-// // type : 'POST',
-
-// // /** success Type: Function( Anything data, String textStatus, jqXHR jqXHR ) */
-// // success : function (data) {
-// // console.log(data);
-// // if (data.error != null) {
-// // displayAlert('danger', 'Erreur!', data.error);
-// // } else {
-// // displayAlert('success', 'Succès!', data.message);
-// // }
-// // },
-
-// // /** error Type: Function( jqXHR jqXHR, String textStatus, String errorThrown ) */
-// // error : function (jqXHR, textStatus, errorThrown) {
-// // displayAlert('danger', 'Erreur!', textStatus);
-// // }
-// // });
-
-// // }
-
-// return false;
-
-// });
+	return false;
+});
 
 $('#avatar-image').change(function (e) {
 	var reader = new FileReader();
@@ -95,10 +62,15 @@ $(document).ready(function () {
 
 	// remplissage des inputs...
 	try {
+		
+		//console.dir(userProfile);
+		
 		if (!!userProfile.firstname)
 			$('#firstname').val(userProfile.firstname);
+		
 		if (!!userProfile.lastname)
 			$('#lastname').val(userProfile.lastname);
+		
 		if (!!userProfile.country)
 			$('#country').val(userProfile.country);
 
@@ -109,13 +81,13 @@ $(document).ready(function () {
 			var dateOfBirth = userProfile['date-of-birth'];
 			$('#date-of-birth').datepicker('update', dateOfBirth);
 		}
-		
-		// image 
-		if (userProfile['filename'] != ''){
+
+		// image
+		if (typeof(userProfile['filename']) != 'undefined') {
 			console.log('filename: ' + userProfile['filename']);
 			$('#profile-picture').attr('src', "/avatars/" + userProfile['filename']);
 		}
-		
+
 	} catch (e) {
 		console.log("exception! " + e);
 	}
